e4ab872272146787b5723d8304e79317562ee00e
2102596sit <2102596@sit.singaporetech.edu.sg>
2023-11-15 12:17:58 +0800

    added

--- app/src/main.py
+++ app/src/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 
 import re
 import secrets
 
 INDEX_PAGE = "index.html"
+RESULTS_PAGE = "results.html"
 SANITISED_PATTERN = re.compile(r"^[a-zA-Z0-9 ]+$")
 
 app = Flask(__name__)
@@ -13,11 +14,9 @@ app.config['SECRET_KEY'] = secrets.token_bytes(64)
 
 csrf = CSRFProtect(app)
 
-
 @app.route('/')
 def index():
     return render_template(INDEX_PAGE)
-
 
 @app.route('/submit', methods=["POST"])
 def submit():
@@ -25,20 +24,23 @@ def submit():
         search = request.form["search"]
 
         if not _check_sanitise(search):
-            return render_template(INDEX_PAGE, result="Nice try"), 400
+            return render_template(INDEX_PAGE, result="Attack detected"), 400
 
-        return render_template(INDEX_PAGE, result=f"Showing results for: {search}"), 200
+        # Redirect to the results page if input is safe
+        return redirect(url_for('results', search_term=search))
     except Exception as e:
         return str(e), 500
 
+@app.route('/results')
+def results():
+    search_term = request.args.get('search_term')
+    return render_template(RESULTS_PAGE, search_term=search_term)
 
 # Sanitise for XSS and SQLi
 def _check_sanitise(password: str) -> bool:
     if SANITISED_PATTERN.match(password):
         return True
-
     return False
-
 
 if __name__ == '__main__':
     app.run(debug=True)
